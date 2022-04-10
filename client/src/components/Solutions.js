81b7bd9a84c21edbf65e9b0689d3ce2007f92c4f
Himanshu <himvarandani@gmail.com>
2022-04-10 23:44:03 +0530

    Update UI for single challenge page with solutions component

--- client/src/components/Solutions.js
+++ client/src/components/Solutions.js
@@ -58,101 +58,93 @@ const Solutions = () => {
   }, [pageNo]);
 
   return (
-    <div className="w-full">
-      <div className="flex items-center flex-col mx-2 mb-4">
-        <div className="flex justify-end mx-10 my-4">
-          <Link to={`/challenge/${challengeId}/create-solution`}>
-            <h2 className="text-center text-pink-700">
-              Create solution
-            </h2>
-          </Link>
-        </div>
-        
-        <div className="w-full border-2">
-          <div className="flex flex-wrap items-between justify-center">
-            {solutions.map((solution) => {
-              const temp = new Date(solution.posted_on);
-              const postedOn_date =
-                temp.getDate() +
-                " " +
-                monthNames[temp.getMonth()] +
-                " " +
-                temp.getFullYear();
-              return (
-                <div
-                  key={solution.solution_id}
-                  className="border-2 shadow-sm hover:shadow-xl rounded-lg m-2 p-4 w-24per md:w-1/2 sm:w-2/3 xs:w-5/6"
-                >
-                  <div className="rounded-lg bg-gradient-to-r from-pink-900 to-blue-grd border-gray-500 border-2 flex flex-col justify-between p-3">
-                    <div className="h-25per flex justify-center">
-                      <h2 className="text-2xl font-mono font-semibold text-white">
-                        {solution.title}
-                      </h2>
-                    </div>
+    <div className="w-full border-2">
+      <div className="flex flex-wrap items-between justify-center">
+        {solutions.map((solution) => {
+          const temp = new Date(solution.posted_on);
+          const postedOn_date =
+            temp.getDate() +
+            " " +
+            monthNames[temp.getMonth()] +
+            " " +
+            temp.getFullYear();
+          return (
+            <div
+              key={solution.solution_id}
+              className="flex flex-col justify-between border-2 shadow-sm hover:shadow-xl rounded-lg m-2 p-4 w-24per md:w-1/2 sm:w-2/3 xs:w-5/6"
+            >
+              <div>
+                <div className="rounded-lg bg-gradient-to-r from-pink-900 to-blue-grd border-gray-500 border-2 flex flex-col justify-between p-3">
+                  <div className="h-25per flex justify-center">
+                    <h2 className="text-2xl font-mono font-semibold text-white">
+                      {solution.title}
+                    </h2>
                   </div>
-                  <div className="p-2 font-serif flex items-center justify-center">
-                    <p>
-                      {!solution.description ? null : (
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: draftToHtml(
-                              getTruncatedContentState(
-                                JSON.parse(solution.description)
-                              )
-                            ),
-                          }}
-                        />
-                      )}
-                    </p>
-                  </div>
-                  <p className="text-xs pl-2 text-gray-400">
-                    Posted on : {postedOn_date}
+                </div>
+                <div className="p-2 font-serif flex items-center justify-center">
+                  <p>
+                    {!solution.description ? null : (
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: draftToHtml(
+                            getTruncatedContentState(
+                              JSON.parse(solution.description)
+                            )
+                          ),
+                        }}
+                      />
+                    )}
                   </p>
-                  <div className="flex flex-col">
-                    <div className="flex flex-col m-4 rounded shadow-lg border-2">
-                      <p className="font-semibold flex justify-center align-bottom">
+                </div>
+              </div>
+              <div>
+                <p className="text-xs pl-2 text-gray-400">
+                  Posted on : {postedOn_date}
+                </p>
+                <div className="flex flex-col">
+                  <div className="flex flex-col m-4 rounded shadow-lg border-2">
+                    <p className="font-semibold flex justify-center align-bottom">
+                      <FontAwesomeIcon
+                        icon={faUser}
+                        size="sm"
+                        className="p-1"
+                      />
+                      Patil
+                    </p>
+                    <div>
+                      <div className="flex justify-center">
                         <FontAwesomeIcon
-                          icon={faUser}
-                          size="sm"
-                          className="p-1"
+                          icon={faEnvelope}
+                          size="lg"
+                          className=" p-1"
                         />
-                        Patil
-                      </p>
-                      <div>
-                        <div className="flex justify-center">
-                          <FontAwesomeIcon
-                            icon={faEnvelope}
-                            size="lg"
-                            className=" p-1"
-                          />
-                          <p className="font-serif">patil@hmail.com</p>
-                        </div>
+                        <p className="font-serif">patil@hmail.com</p>
                       </div>
                     </div>
                   </div>
-                  <div>
-                    <Link to={`/solution/${solution.solution_id}`}>
-                      <h2 className="text-center p-4 text-pink-700">
-                        View Detail
-                      </h2>
-                    </Link>
-                  </div>
                 </div>
-              );
-            })}
-          </div>
-          {moreSolutionsAvlbl && loading ? (
-            <div className="flex justify-center w-full my-20">
-              <FontAwesomeIcon
-                icon={faSpinner}
-                size="4x"
-                color="pink"
-                spin={true}
-              />
+                <div>
+                  <Link to={`/solution/${solution.solution_id}`}>
+                    <h2 className="text-center p-4 text-pink-700">
+                      View Detail
+                    </h2>
+                  </Link>
+                </div>
+              </div>
             </div>
-          ) : null}
-        </div>
+          );
+        })}
       </div>
+      {moreSolutionsAvlbl && loading ? (
+        <div className="flex justify-center w-full my-20">
+          <FontAwesomeIcon
+            icon={faSpinner}
+            size="4x"
+            color="pink"
+            spin={true}
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
--- client/src/pages/challenges/Challenge.js
+++ client/src/pages/challenges/Challenge.js
@@ -1,6 +1,7 @@
 import draftToHtml from "draftjs-to-html";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import Solutions from "../../components/Solutions";
@@ -17,7 +18,6 @@ const Challenge = () => {
     if (challengeId)
       getSingleChallenge(challengeId)
         .then(({ data }) => {
-          console.log(data);
           setChallenge(data);
         })
         .catch(() => {});
@@ -26,11 +26,11 @@ const Challenge = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex items-center flex-col mx-2 mb-4 ">
-        <div className="flex flex-col sm:w-full w-full mb-1 border-2  ">
-          <div className="bg-red-500 h-30v flex items-center justify-center">
+      <div className="flex items-center flex-col mx-10 mb-10">
+        <div className="flex flex-col mb-1">
+          <div className="h-30v flex items-center justify-center">
             <img
-              className="object-fill h-full w-full"
+              className="object-fill h-full rounded-3xl"
               src={
                 !challenge.cover_image
                   ? challenge_cover
@@ -40,31 +40,35 @@ const Challenge = () => {
             />
           </div>
         </div>
-        <h1 className="mb-5 flex justify-center font-serif text-4xl">
+        <h1 className="my-5 flex justify-center font-serif text-4xl">
           {challenge.title}
         </h1>
         
-        <div className="flex justify-start w-full">
+        <div className="flex justify-between w-full mb-5">
           <div className="flex w-80v flex-wrap md:w-95v sm:w-95v">
             <button
-              className="bg-gray-300 p-2 mb-1 border-white border-x"
+              className={`${tab == "overview" ? "border-2" : "border-b-2 hover:border-2"} border-black hover:scale-110 p-2 mr-2`}
               onClick={() => setTab("overview")}
             >
               Overview
             </button>
             <button
-              className="bg-gray-200 p-2 mb-1"
+              className={`${tab == "solutions" ? "border-2" : "border-b-2 hover:border-2"} border-black hover:scale-110 p-2 mx-2`}
               onClick={() => setTab("solutions")}
             >
               Solutions
             </button>
-            <button
-              className="bg-gray-200 p-2 mb-1"
-              onClick={() => setTab("comments")}
-            >
-              Comments
-            </button>
           </div>
+          
+          {tab == "solutions" ? (
+            <div className="flex justify-end">
+              <Link to={`/challenge/${challengeId}/create-solution`}>
+                <h2 className="border-b-2 hover:border-2 border-black hover:scale-110 text-center text-pink-700 p-2">
+                  Create solution
+                </h2>
+              </Link>
+            </div>
+          ) : null}
         </div>
 
         {tab == "overview" ? (
