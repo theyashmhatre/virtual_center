7fb6c16c7b4a13a0a8467c90dd36f4c7ce99a024
Himanshu <himvarandani@gmail.com>
2022-04-12 00:52:13 +0530

    Update pagination on scrolling to end in offerings page

--- client/src/pages/offerings/Offerings.js
+++ client/src/pages/offerings/Offerings.js
@@ -1,4 +1,4 @@
-import { faEnvelope, faPaperclip, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPaperclip, faSpinner, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import draftToHtml from "draftjs-to-html";
 import { useEffect, useState } from "react";
@@ -12,16 +12,47 @@ const Offerings = () => {
   const [errors, setErrors] = useState({});
   const [offeringData, setOfferingData] = useState([]);
   const [pageNo, setPageNo] = useState(1);
-  const limit = 8;
+  const [loading, setLoading] = useState(true);
+  const [moreOfferingsAvlbl, setMoreOfferingsAvlbl] = useState(true);
+  const limit = 2;
+
+  const handleScroll = (e) => {
+    const window = e.currentTarget;
+    if (
+      window.scrollY > (
+        window.document.documentElement.scrollHeight -
+        window.document.documentElement.clientHeight -
+        100
+      )
+    )
+      setPageNo(prevPageNo => prevPageNo+1);
+  };
 
   useEffect(() => {
+    if (!moreOfferingsAvlbl) return;
+
+    window.addEventListener("scroll", (e) => handleScroll(e));
+
+    return () => {
+      window.removeEventListener("scroll", (e) => handleScroll(e));
+    };
+  }, [window.scrollY]);
+  
+  useEffect(() => {
+    if (!moreOfferingsAvlbl) return;
+
     getOfferings(pageNo, limit)
       .then(({ data }) => {
         if (data.offerings_count)
           setOfferingData([...offeringData, ...data.offerings_list]);
+        
+        setLoading(false);
+        if (data.offerings_count < limit)
+          setMoreOfferingsAvlbl(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false);
       });
   }, [pageNo]);
 
@@ -29,16 +60,19 @@ const Offerings = () => {
     <div className="">
       <Navbar />
 
-      <div className=" flex justify-center lg:w-4/5 ">
-        <div className="lg:h-80v  w-90v">
-          <div className="lg:h-70v lg:w-4/5 flex md:flex-col md:items-center sm:items-center sm:flex-col justify-between flex-wrap">
+      <div className="min-h-screen mx-16 my-5">
+        <h1 className="text-3xl text-center font-bold mb-10">
+          Offerings
+        </h1>
+        <div className="lg:h-80v w-90v">
+          <div className="lg:h-70v lg:w-4/5 flex md:flex-col md:items-center sm:items-center sm:flex-col justify-start flex-wrap">
             {/* Offerings blocks Started */}
 
             {offeringData.map((data, id) => {
               return (
                 <div
                   key={id}
-                  className="border-2 shadow-sm hover:shadow-xl rounded-lg lg:mb-0 mb-4 w-24per  md:w-1/2 sm:w-2/3 xs:w-5/6"
+                  className="border-2 shadow-sm hover:shadow-xl rounded-lg lg:mb-0 mb-4 mx-2 w-24per  md:w-1/2 sm:w-2/3 xs:w-5/6"
                 >
                   <div className="rounded-lg bg-gradient-to-r from-pink-900 to-blue-grd border-gray-500 border-2 flex flex-col justify-between p-3">
                     <div className="h-25per flex justify-center">
@@ -104,14 +138,16 @@ const Offerings = () => {
               );
             })}
           </div>
-          <div className="flex justify-center mt-5 mb-10">
-            <button
-              className="text-lg text-red-700 font-bold"
-              onClick={() => setPageNo(pageNo + 1)}
-            >
-              Load More
-            </button>
-          </div>
+          {moreOfferingsAvlbl && loading ? (
+            <div className="flex justify-center w-full my-20">
+              <FontAwesomeIcon
+                icon={faSpinner}
+                size="4x"
+                color="pink"
+                spin={true}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
       <Footer />
