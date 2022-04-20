ddbb13f2b1853b35339202fdb6707ae8e59813c1
Himanshu <himvarandani@gmail.com>
2022-04-21 02:10:56 +0530

    feat: Integrate most submissions done on dashboard with backend

--- client/src/utilities/api/dashboard.js
+++ client/src/utilities/api/dashboard.js
@@ -3,4 +3,9 @@ import api from "./index";
 export const getChallengesCounts = async () => {
   const endpoint = "/api/dashboard/account-analytics";
   return await api.get(endpoint);
-}
+};
+
+export const getMostSubmissions = async (year) => {
+  const endpoint = `/api/dashboard/most-submissions/${year}`;
+  return await api.get(endpoint);
+};
