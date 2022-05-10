f66e29f3e678b24edb32f33bbf1345415ee22836
Himanshu <himvarandani@gmail.com>
2022-05-10 19:06:54 +0530

    fix: Make sidebar and navbar responsive

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="min-h-screen fixed w-24 shadow-md bg-neutral-800 pt-4 px-0">
+    <div className="h-full w-full shadow-md bg-neutral-800 pt-4 px-0">
       {/* sidebar */}
       <ul className="flex flex-col gap-9 pr-2">
         <li className="left">
@@ -15,9 +15,9 @@ const Sidebar = () => {
               backgroundColor: isActive ? "rgb(190 24 93)" : "",
             })}
             to="/main"
-            className="flex items-center justify-center py-4 px-6 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
+            className="flex items-center justify-center py-4 md:py-3 sm:py-2 px-6 md:px-4 sm:px-2 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
           >
-            <AiOutlineHome size={40}/>
+            <AiOutlineHome size={40} />
           </NavLink>
         </li>
         <li className="left">
@@ -26,7 +26,7 @@ const Sidebar = () => {
               backgroundColor: isActive ? "rgb(190 24 93)" : "",
             })}
             to="/challenge"
-            className="flex items-center justify-center py-4 px-6 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
+            className="flex items-center justify-center py-4 md:py-3 sm:py-2 px-6 md:px-4 sm:px-2 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
           >
             <IoIosCalendar size={40} />
           </NavLink>
@@ -37,9 +37,9 @@ const Sidebar = () => {
               backgroundColor: isActive ? "rgb(190 24 93)" : "",
             })}
             to="/support"
-            className="flex items-center justify-center py-4 px-6 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
+            className="flex items-center justify-center py-4 md:py-3 sm:py-2 px-6 md:px-4 sm:px-2 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
           >
-            <BsTelephone size={30}/>
+            <BsTelephone size={30} />
           </NavLink>
         </li>
         <li className="left">
@@ -48,9 +48,9 @@ const Sidebar = () => {
               backgroundColor: isActive ? "rgb(190 24 93)" : "",
             })}
             to="/help"
-            className="flex items-center justify-center py-4 px-6 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
+            className="flex items-center justify-center py-4 md:py-3 sm:py-2 px-6 md:px-4 sm:px-2 h-20 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:bg-pink-700 transition duration-300 ease-in-out rounded-r-lg"
           >
-            <FaRegCommentAlt size={30}/>
+            <FaRegCommentAlt size={30} />
           </NavLink>
         </li>
       </ul>
