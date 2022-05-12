b9ca57f0d82818d2b65edb7d1f501e30ce36697d
Himanshu <himvarandani@gmail.com>
2022-05-12 18:21:23 +0530

    Make seperate file for super admin api's

--- client/src/utilities/api/userDetails.js
+++ client/src/utilities/api/userDetails.js
@@ -1,35 +1,5 @@
 import api from "./index";
 
-export const getUserDetails = () => {
-  const endpoint = "/api/user/get-users";
-  return api.get(endpoint);
-};
-
-export const getAdminDetails = () => {
-  const endpoint = "/api/user/get-admins";
-  return api.get(endpoint);
-};
-
-export const updateStatusToActive = (username) => {
-  const endpoint = `/api/user/active/${username}`;
-  return api.post(endpoint);
-};
-
-export const updateStatusToInactive = (username) => {
-  const endpoint = `/api/user/inactive/${username}`;
-  return api.post(endpoint);
-};
-
-export const changeUserToAdmin = (username) => {
-  const endpoint = `/api/user/change-to-admin/${username}`;
-  return api.post(endpoint);
-};
-
-export const changeAdminToUser = (username) => {
-  const endpoint = `/api/user/change-to-user/${username}`;
-  return api.post(endpoint);
-};
-
 export const profile = async (userId) => {
   const endpoint = `/api/user/profile/${userId}`;
   return await api.get(endpoint);
