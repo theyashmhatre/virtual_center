fd91f661772f8e90f055604c341685ea605e81ae
Himanshu <himvarandani@gmail.com>
2022-05-13 02:39:46 +0530

    Update super admin endpoints and responsive settings page

--- client/src/utilities/api/superAdmin.js
+++ client/src/utilities/api/superAdmin.js
@@ -1,31 +1,31 @@
 import api from "./index";
 
 export const getUsers = () => {
-  const endpoint = "/api/user/get-users";
+  const endpoint = "/api/super-admin/get-users";
   return api.get(endpoint);
 };
 
 export const getAdmins = () => {
-  const endpoint = "/api/user/get-admins";
+  const endpoint = "/api/super-admin/get-admins";
   return api.get(endpoint);
 };
 
 export const updateStatusToActive = (username) => {
-  const endpoint = `/api/user/active/${username}`;
+  const endpoint = `/api/super-admin/active/${username}`;
   return api.post(endpoint);
 };
 
 export const updateStatusToInactive = (username) => {
-  const endpoint = `/api/user/inactive/${username}`;
+  const endpoint = `/api/super-admin/inactive/${username}`;
   return api.post(endpoint);
 };
 
 export const changeUserToAdmin = (username) => {
-  const endpoint = `/api/user/change-to-admin/${username}`;
+  const endpoint = `/api/super-admin/change-to-admin/${username}`;
   return api.post(endpoint);
 };
 
 export const changeAdminToUser = (username) => {
-  const endpoint = `/api/user/change-to-user/${username}`;
+  const endpoint = `/api/super-admin/change-to-user/${username}`;
   return api.post(endpoint);
 };
